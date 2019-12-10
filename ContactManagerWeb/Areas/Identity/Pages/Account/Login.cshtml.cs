07b949f49b00f69d26e14b943c8b1b28a6b7fa68
jakecook1 <jake.cook1@gmail.com>
2019-12-10 16:16:48 +1300

    General check code not up to date

--- ContactManagerWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ContactManagerWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -65,7 +65,7 @@ namespace ContactManagerWeb.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Contacts");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -77,7 +77,7 @@ namespace ContactManagerWeb.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Contacts");
 
             if (ModelState.IsValid)
             {
